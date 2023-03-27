c21eef27259e75a8bb4b91eef65fd61746c0d98b
george woods <georgewoods01@gmail.com>
2023-03-27 13:02:04 +0200

    deleted some files, added two recipe components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import RecipeList from './components/recipes/RecipeList';
+
+const DUMMY_DATA = [
+
+  {
+    id: 'r1',
+    title: 'BibimBap',
+    image: 'https://www.seriouseats.com/thmb/KHuz-5lNfyMtGEB1K5YtuXhMAdg=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/20230113-Bibimbap-AmandaSuarez-hero-331e5e1ffa5b400fbb684e59b14d57c1.JPG',
+    description: 'Put rice and things in a bowl'
+  },
+  {
+    id: 'r2',
+    title: 'Fried Chicken',
+    image: 'https://christieathome.com/wp-content/uploads/2020/10/Facetune_06-10-2020-15-37-58-scaled.jpg',
+    description: 'Fry the chicken'
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <RecipeList recipes={DUMMY_DATA} />
+
   );
 }
 
